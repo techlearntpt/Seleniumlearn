a162e4a522e58bd9fb7cc515ee80c9dcee845b13
techlearntpt <155513527+techlearntpt@users.noreply.github.com>
2024-01-03 11:37:17 +0530

    Update SecondProgram.java

--- src/javademo/SecondProgram.java
+++ src/javademo/SecondProgram.java
@@ -5,7 +5,7 @@ public class SecondProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello GitHub");
+		System.out.println("Hello Selenium");
 
 	}
 
